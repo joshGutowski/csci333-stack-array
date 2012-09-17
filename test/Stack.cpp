970f7995e91e74e71203973d7a9b3b5252e29e59
jgutowsk <jgutowsk@unca.edu>
2012-09-16 20:46:52 -0400

    Modified Stack.cpp

--- test/Stack.cpp
+++ test/Stack.cpp
@@ -13,5 +13,12 @@ TEST(StackTest, PushOver) {
   Stack* s = new Stack(2);
   s->push(1);
   s->push(2);
-  EXPECT_EQ(2, s->size());
+  s->push(3);
+  s->push(4);
+  s->push(5);
+  s->push(6);
+  s->push(7);
+  s->push(8);
+  EXPECT_EQ(8, s->size());
+  delete s;
 }
